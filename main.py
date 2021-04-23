af46772044a1035371a1f93be0cae6d63f6afe08
Andy Atari <andrewsalteri@gmail.com>
2021-04-23 12:19:49 -0500

    Added image and displayed image as icon next to screen caption.

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("PyInvader")
+icon = pygame.image.load("ufo.png")
+pygame.display.set_icon(icon)
 
 running = True 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False  
+
+# <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-buddha" title="Pixel Buddha">Pixel Buddha</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
