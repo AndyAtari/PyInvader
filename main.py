b74fa4fd1d8ea7b742deb273966df73250561b3b
Andy Atari <andrewsalteri@gmail.com>
2021-05-01 18:18:25 -0400

    Added an image for the player's ship and called a method to draw that image on the game screen.

--- main.py
+++ main.py
@@ -8,15 +8,30 @@ pygame.display.set_caption("PyInvader")
 icon = pygame.image.load("ufo.png")
 pygame.display.set_icon(icon)
 
-# Loop for keeping window open until clicking X. Changes screen color and updates while loop is running. 
+# Player 1
+playerImg = pygame.image.load('player1.png')
+playerX = 370
+playerY = 500
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
+# Loop for keeping window open until closing. Draws screen and updates screen. 
 running = True 
 while running:
+
+    screen.fill((221, 160, 221))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False  
 
-    screen.fill((221, 160, 221))
+    
+    
+    player()
     pygame.display.update()
 
-# <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-buddha" title="Pixel Buddha">Pixel Buddha</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
+# <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-buddha" title="Pixel Buddha">Pixel Buddha</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+# <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> 
