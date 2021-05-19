716484ed3b7ad02c5897bf7b513fd46ac1770a06
Andy Atari <andrewsalteri@gmail.com>
2021-05-18 20:27:26 -0400

    Added for loops that create 10 invaders on screen at all times.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import math
 
 pygame.init()
 
+# Initial Score 
 score = 0
 
 # set display screen size and added caption and icon
@@ -26,14 +27,26 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 # Invader
-invaderImg = pygame.image.load('ufo (1).png')
-INVADER_X = random.randint(0, 736)
-INVADER_Y = random.randint(50, 200)
-INVADERX_MOVE = 0.1
-INVADERY_MOVE = 20
+invaderImg = []
+invaderX = []
+invaderY = []
+invaderX_move = []
+invaderY_move = []
+num_of_invaders = 10
 
-def invader(x, y):
-    screen.blit(invaderImg, (x, y))
+# Drawing Invaders 
+for i in range(num_of_invaders):
+
+    invaderImg.append(pygame.image.load('ufo (1).png'))
+    invaderX.append(random.randint(0, 736))
+    invaderY.append(random.randint(50, 200))
+    invaderX_move.append(0.2)
+    invaderY_move.append(20)
+
+def invader(x, y, i):
+    screen.blit(invaderImg[i], (x, y))
+
+# Explosions 
 
 explosionImg = pygame.image.load('explosion2.png')
 
@@ -52,8 +65,8 @@ def fire_bullet(x,y):
     bullet_state = "fire"
     screen.blit(bulletImg, (x + 16,y + 10))
 
-def isCollision(INVADER_X, INVADER_Y, bulletX, bulletY):
-    distance = math.sqrt(math.pow(INVADER_X - bulletX,2) + math.pow(INVADER_Y - bulletY,2))
+def isCollision(invaderX, invaderY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(invaderX - bulletX,2) + math.pow(invaderY - bulletY,2))
     if distance < 32:
         return True
     else:
@@ -95,14 +108,28 @@ while running:
         playerX = 736
 
     # BOUNDARY CHECK/MOVEMENT OF INVADER 
-    INVADER_X += INVADERX_MOVE
+    for i in range(num_of_invaders):
+        invaderX[i] += invaderX_move[i]
 
-    if INVADER_X <= 0:
-        INVADERX_MOVE = 0.1
-        INVADER_Y += INVADERY_MOVE
-    elif INVADER_X >= 736:
-        INVADERX_MOVE = -0.1
-        INVADER_Y += INVADERY_MOVE
+        if invaderX[i] <= 0:
+            invaderX_move[i] = 0.2
+            invaderY[i] += invaderY_move[i]
+        elif invaderX[i] >= 736:
+            invaderX_move[i] = -0.2
+            invaderY[i] += invaderY_move[i]
+
+        collision = isCollision(invaderX[i], invaderY[i], bulletX, bulletY)
+        if collision:
+            explosion(invaderX[i], invaderY[i])
+            bulletY = 500
+            bullet_state = "ready"
+            score += 100
+            print(score)
+            invaderX[i] = random.randint(0, 736)
+            invaderY[i] = random.randint(50, 200)
+
+        invader(invaderX[i], invaderY[i], i)
+        
 
     # Bullet Movement
     if bulletY <= 0:
@@ -111,27 +138,9 @@ while running:
 
     if bullet_state == "fire":
         fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
-
-    # Collision
-    collision = isCollision(INVADER_X, INVADER_Y, bulletX, bulletY)
-    if collision:
-        explosion(INVADER_X, INVADER_Y)
-        bulletY = 500
-        bullet_state = "ready"
-        score += 100
-        print(score)
-        INVADER_X = random.randint(0, 736)
-        INVADER_Y = random.randint(50, 200)
-
-    
-        
-       
-        
-        
+        bulletY -= bulletY_change  
     
     player(playerX, playerY)
-    invader(INVADER_X, INVADER_Y)
     pygame.display.update()
 
 
